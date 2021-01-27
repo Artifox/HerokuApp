9d49bb40ead08680e76a537195c81ad0c83eb6d6
AKoptsiukh <sid_93@mail.ru>
2021-01-27 08:50:33 +0300

    Initial Commit

---
+++ src/test/java/AddRemoveElementsTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class AddRemoveElementsTest {
+
+    @Test
+    public void amountOfDeleteButtonsTest(){
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement addElementButton = driver.findElement(By.xpath("//button[text()='Add Element']"));
+        addElementButton.click();
+        addElementButton.click();
+        driver.findElements(By.xpath("//button[text()='Delete']")).get(1).click();
+        int numberOfButtons = driver.findElements(By.xpath("//button[text()='Delete']")).size();
+        Assert.assertEquals(numberOfButtons, 1, "There are less Delete buttons than expected ");
+        driver.quit();
+    }
+}
---
+++ src/test/java/HerokuAppTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HerokuAppTest {
+
+    //classwork
+    @Test
+    public void siteShouldByOpened(){
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        /*System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();*/
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().window().setSize(new Dimension(1024, 768));
+        driver.get("http://the-internet.herokuapp.com/");
+        //driver.navigate().
+        driver.quit();
+    }
+}
